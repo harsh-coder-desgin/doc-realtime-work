4b8534796477fa664416159a0b30696a8ba31393
harsh-coder-desgin <harshpatel51057@gmail.com>
2026-01-30 19:30:09 +0530

    All middelware added

---
+++ doc/src/middlewares/usertoken.middleware.js
@@ -0,0 +1,27 @@
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/ApiError.js";
+// import { asyncHandler } from "../utils/asyncHandler.js";
+// import jwt from "jsonwebtoken"
+
+export const verifyJWT = asyncHandler(async(req, res, next) => {
+    try {
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","")
+
+        if(!token){
+            throw new ApiError(401,"unauthorized request")
+        }
+        
+        const decodedtoken = jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
+    
+        const users = await User.findById(decodedtoken?._id).select("-password -refreshToken")
+        
+        if(!users){
+            throw new ApiError(401,"invaild access token here")
+        }
+    
+        req.users = users;
+        next()
+    } catch (error) {
+        throw new ApiError( 401,"Invaild access token")
+    }
+})
